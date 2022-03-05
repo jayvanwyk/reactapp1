d273931690fa6046acd10bfd6e999c5b3a705364
Jay van Wyk <jayvanwyk@gmail.com>
2022-03-05 13:00:24 +0000

    Up to start of assignment 1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,47 @@
+import React from 'react';
+import ExpenseItem from './components/ExpenseItem';
+
 function App() {
+  const expenses = [
+    {
+      index: 1,
+      title: 'Car Insurance',
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      index: 2,
+      title: 'Home Insurance',
+      amount: 59.83,
+      date: new Date(2021, 1, 2),
+    },
+    {
+      index: 3,
+      title: 'Pet Insurance',
+      amount: 498.67,
+      date: new Date(2021, 2, 3),
+    },
+    {
+      index: 4,
+      title: 'Adventure Sports Insurance',
+      amount: 586.67,
+      date: new Date(2021, 3, 8),
+    },
+  ];
+
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <h2>Lets get started</h2>
+      {expenses.map((item) => {
+        return (
+          <ExpenseItem
+            expenseDate={item.date}
+            expenseTitle={item.title}
+            expenseAmount={item.amount}
+            key={item.index}
+          />
+        );
+      })}
     </div>
   );
 }
---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,16 @@
+import ExpenseDate from './ExpenseDate';
+import './ExpenseItem.css';
+
+function ExpenseItem(props) {
+  return (
+    <div className="expense-item">
+      <ExpenseDate date={props.expenseDate} />
+      <div className="expense-item__description">
+        <h2>{props.expenseTitle}</h2>
+        <div className="expense-item__price">${props.expenseAmount}</div>
+      </div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
